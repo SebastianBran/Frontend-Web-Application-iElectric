2abb16dbf7b4975d44e3dfe2a5ff91faf4e30ad7
Sebastian <s.gamarra9977@hotmail.com>
2021-09-27 11:33:06 -0500

    feat: I added CRUD for the appointments

---
+++ src/core/services/appointments-api-service.js
@@ -0,0 +1,24 @@
+import http from './http-common'
+
+class AppointmentsApiService {
+    getAll() {
+        return http.get('/appointments');
+    }
+
+    getById(id) {
+        return http.get(`/appointments/${ id }`);
+    }
+
+    create(data) {
+        return http.post('/appointments', data);
+    }
+
+    update(id, data) {
+        return http.put(`/appointments/${ id }`, data);
+    }
+
+    delete(id) {
+        return http.delete(`/appointments/${ id }`);
+    }
+}
+export default  new AppointmentsApiService();

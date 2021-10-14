8b30ff93bce4074999db0dad0956d334bf66af7b
Nicolas Hernandez <bijusamma@gmail.com>
2021-10-13 23:03:53 -0500

    feat: Handle Technicians
    I added the CRUD, forms and validations to handle a technician from the administrator view

--- src/views/administrator/Technicians.vue
+++ src/views/administrator/Technicians.vue
@@ -1,15 +1,341 @@
 <template>
-  <div class="orange">
-    technicians
+  <div>
+    <h1 class="mb-2">Technicians</h1>
+    <v-btn
+        color="accent"
+        class="mb-4"
+        @click="seeAddDialog()"
+    >
+      Add
+    </v-btn>
+
+    <!-- Table Technicians - START -->
+    <v-data-table
+        :headers="headers"
+        :items="technicians"
+        :items-per-page="10"
+        class="elevation-1"
+        :search="search"
+    >
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+            small
+            class="mr-2"
+            @click="seeEditDialog(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+            small
+            class="mr-2"
+            @click="seeDeleteDialog(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
+    <!-- Table Technicians - END -->
+
+    <!-- Dialog see Technician - START-->
+    <v-dialog
+        v-model="openTechnician"
+        max-width="600px"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="text-h5"> Technician </span>
+        </v-card-title>
+
+        <v-card-text>
+          <span v-if="isCreate === false">
+            Last Time Connected:
+            {{new Date(editItem.lastTimeConnected).getDate()}} -
+            {{new Date(editItem.lastTimeConnected).getMonth()}} -
+            {{new Date(editItem.lastTimeConnected).getFullYear()}}
+
+          </span>
+          <v-container>
+            <v-form ref="form" v-model="valid">
+              <v-text-field
+                  v-model="editItem.names"
+                  label="Name"
+                  :counter="20"
+                  required
+                  :rules="nameRules"
+                  id="names"
+                  aria-labelledby="names"
+              ></v-text-field>
+              <v-text-field
+                  v-model="editItem.lastnames"
+                  label="Lastname"
+                  :counter="20"
+                  required
+                  :rules="lastNameRules"
+                  id="lastnames"
+                  aria-labelledby="lastnames"
+              ></v-text-field>
+              <v-text-field
+                  v-model="editItem.email"
+                  label="Email"
+                  required
+                  :rules="emailRules"
+                  id="email"
+                  aria-labelledby="email"
+              ></v-text-field>
+              <v-text-field
+                  v-model="editItem.password"
+                  label="Password"
+                  :counter="15"
+                  :rules="passwordRules"
+                  required
+                  id="password"
+                  aria-labelledby="password"
+              ></v-text-field>
+              <v-text-field
+                  v-model="editItem.cellphoneNumber"
+                  label="Cellphone"
+                  :counter="15"
+                  required
+                  :rules="phoneRules"
+                  id="cellphoneNumber"
+                  aria-labelledby="cellphoneNumber"
+              ></v-text-field>
+              <v-text-field
+                  v-model="editItem.address"
+                  label="Address"
+                  :counter="50"
+                  required
+                  :rules="addressRules"
+                  id="address"
+                  aria-labelledby="address"
+              ></v-text-field>
+
+              <v-menu
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+              >
+                <template v-slot:activator="{on, attrs}">
+                  <v-text-field
+                      v-model="editItem.birthday"
+                      label="Birthday"
+                      id="birthday"
+                      aria-labelledby="birthday"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                    v-model="editItem.birthday"
+                    @input="menu = false"
+                ></v-date-picker>
+              </v-menu>
+            </v-form>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              text
+              color="primary"
+              @click="openTechnician = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+              v-if="isCreate"
+              text
+              color="primary"
+              :disabled="!valid"
+              @click="AddTechnician()"
+          >
+            Add
+          </v-btn>
+          <v-btn
+              v-else
+              text
+              color="primary"
+              :disabled="!valid"
+              @click="updateTechnician(editItem.id, editItem)"
+          >
+            Update
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- Dialog see Technician - END-->
+
+    <!--  Delete Dialog - START-->
+    <v-dialog
+        v-model="openDelete"
+        max-width="350px"
+    >
+        <v-card>
+          <v-container>
+            <v-card-subtitle>
+              <span>Do you want delete this item?</span>
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  text
+                  color="primary"
+                  @click="openDelete = false"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                  text
+                  color="primary"
+                  @click="deleteTechnician(editItem.id)"
+              >
+                Delete
+              </v-btn>
+            </v-card-actions>
+          </v-container>
+        </v-card>
+    </v-dialog>
+    <!-- Delete Dialog - END-->
+
   </div>
 </template>
 
 <script>
+import TechnicianApiService from "../../core/services/technicians-api-service";
+
 export default {
-  name: "Technicians"
+  name: "Technicians",
+  data() {
+    return {
+      technicians: [],
+      search: '',
+      headers: [
+        {text: 'Name', sortable: true, value: 'names'},
+        {text: 'Last Name', sortable: true, value: 'lastnames'},
+        {text: 'Email', sortable: true, value: 'email'},
+        {text: 'Actions', sortable: false, value: 'actions'}
+      ],
+      openTechnician: false,
+      isCreate: false,
+      openDelete: false,
+      editItem: {
+        names: '',
+        lastnames: '',
+        cellphoneNumber: '',
+        address: '',
+        email: '',
+        birthday: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        password: '',
+      },
+      valid: true,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 20) || 'Name must be less than 20 characters',
+      ],
+      lastNameRules: [
+        v => !!v || 'Lastname is required',
+        v => (v && v.length <= 20) || 'Lastname must be less than 20 characters',
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length <= 15) || 'Password must be less than 15 characters',
+      ],
+      addressRules: [
+        v => !!v || 'Address is required',
+        v => (v && v.length <= 50) || 'Address must be less than 50 characters',
+      ],
+      phoneRules: [
+        v => !!v || 'Cellphone is required',
+        v => Number.isInteger(Number(v)) || 'Cellphone value must be an integer number',
+        v => (v && v.length <= 15) || 'Cellphone must be less than 15 characters',
+      ],
+      menu: false,
+    }
+  },
+  methods: {
+    getTechnician(technician) {
+      return {
+        id: technician.id,
+        names: technician.names,
+        lastnames: technician.lastnames,
+        cellphoneNumber: technician.cellphoneNumber,
+        address: technician.address,
+        email: technician.email,
+        birthday: technician.birthday,
+        password: technician.password,
+        lastTimeConnected: technician.lastTimeConnected
+      }
+    },
+    async retrieveTechnicians() {
+      await TechnicianApiService.getAll()
+      .then(response => {
+        this.technicians = response.data.map(this.getTechnician);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    },
+    seeAddDialog() {
+      this.isCreate = true;
+      this.openTechnician = true;
+      this.$refs.form.reset();
+    },
+    async AddTechnician() {
+      await TechnicianApiService.create(this.editItem)
+      .then(() => {
+        this.retrieveTechnicians();
+        this.openTechnician = false;
+        this.isCreate = false;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    },
+    seeEditDialog(item){
+      this.editItem = Object.assign({}, item);
+      this.isCreate = false;
+      this.editItem.birthday = (new Date(this.editItem.birthday)).toISOString().substr(0, 10),
+      this.openTechnician = true;
+    },
+    async updateTechnician(id, item) {
+      await TechnicianApiService.update(id, item)
+      .then(() =>{
+        this.retrieveTechnicians();
+        this.openTechnician = false;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    },
+    seeDeleteDialog(item){
+      this.editItem = Object.assign({}, item);
+      this.openDelete = true;
+    },
+    async deleteTechnician(id) {
+      await TechnicianApiService.delete(id)
+      .then(() => {
+        this.retrieveTechnicians();
+        this.openDelete = false;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    }
+  },
+  mounted() {
+    this.retrieveTechnicians();
+  }
 }
 </script>
 
 <style scoped>
-
 </style>

be6bfeb1a869f471d1278fd4cc2bf52eb5e3caa9
Sebastian <s.gamarra9977@hotmail.com>
2021-10-18 20:23:59 -0500

    fix: Reports section

--- src/views/technician/Reports.vue
+++ src/views/technician/Reports.vue
@@ -25,6 +25,81 @@
         </v-row>
       </v-container>
     </v-card>
+    <div class="btn-add" >
+      <v-btn
+          class="primary"
+          fab
+          dark
+          @click="addNewReport()"
+      >
+        <v-icon dark>
+          mdi-plus
+        </v-icon>
+      </v-btn>
+    </div>
+    <!-- New Report -->
+    <v-dialog
+        v-model="newReport"
+        max-width="600px"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="text-h5"> New Report</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form
+            ref="form"
+          >
+            <v-text-field
+                v-model="formNewReport.observation"
+                ref="textField"
+                label="Observation"
+                clearable
+            ></v-text-field>
+
+            <v-text-field
+                v-model="formNewReport.diagnosis"
+                ref="textField"
+                label="Diagnosis"
+                clearable
+            ></v-text-field>
+
+            <v-text-field
+                v-model="formNewReport.repairDescription"
+                ref="textField"
+                clearable
+                label="Repair Description"
+            ></v-text-field>
+            <v-text-field
+            v-model="formNewReport.date"
+            ref="textField"
+            clearable
+            label="Date"
+            >
+            </v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              text
+              color="primary"
+              @click="registerReport"
+          >
+            Submit
+          </v-btn>
+          <v-btn
+              text
+              color="primary"
+              @click="newReport = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
     <!-- Dialgo see report -->
     <v-dialog
         v-model="openReport"
@@ -87,6 +162,8 @@
 
 <script>
 import TechnicianApiService from "../../core/services/technicians-api-service";
+import ReportsApiService from "../../core/services/reports-api-service";
+import {v4 as uuidv4} from "uuid";
 export default {
   name: "Reports",
   data() {
@@ -95,6 +172,8 @@ export default {
       reports: [],
       search: '',
       openReport: false,
+      newReport:false,
+      e6:1,
       editItem: {
         appointmentId: "",
         date: "",
@@ -106,6 +185,12 @@ export default {
         repairDescription: "",
         technicianId: ""
       },
+      formNewReport:{
+        observation:"",
+        diagnosis:"",
+        repairDescription: "",
+        date:""
+      }
     }
   },
   methods: {
@@ -149,7 +234,31 @@ export default {
     seeReport(item) {
       this.editItem = Object.assign({}, item);
       this.openReport = true;
-    }
+    },
+    addNewReport(){
+      this.newReport=true;
+    },
+    async registerReport(){
+      const newReport={
+        id:uuidv4(),
+        observation:this.formNewReport.observation,
+        diagnosis:this.formNewReport.diagnosis,
+        repairDescription:this.formNewReport.repairDescription,
+        date:this.formNewReport.date,
+        imagePath: "image.png",
+        appointmentId: "1",
+        technicianId: "1"
+      }
+      await ReportsApiService.create(newReport)
+          .then(response=>{
+            console.log(response);
+            this.newReport=false;
+            this.$refs.form.reset();
+          })
+          .catch(e=>{
+          console.log(e);
+          });
+    },
   },
   mounted() {
     this.retrieveReports();
@@ -158,5 +267,10 @@ export default {
 </script>
 
 <style scoped>
-
+.btn-add{
+  display: flex;
+  min-height: 80px;
+  align-items: center;
+  justify-content: center;
+}
 </style>

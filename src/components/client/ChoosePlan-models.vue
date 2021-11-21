df17e8e3bb929b8126b7d97fa3d4a5ca8dae18e0
SebastianBran <sebastianbranzapata@gmail.com>
2021-11-21 14:34:54 -0500

    feat: api consumption for client plans

--- src/components/client/ChoosePlan-models.vue
+++ src/components/client/ChoosePlan-models.vue
@@ -4,20 +4,17 @@
             <CardPlan 
                 v-bind:item="plans[0]" 
                 v-bind:subscription="subscriptionModelItem"
-                v-on:model-choose-plan="/*saveInformationPlan*/"
-                v-bind:clientId="clientId"
+                v-on:model-choose-plan="saveInformationPlan"
             />
             <CardPlan 
                 v-bind:item="plans[1]" 
                 v-bind:subscription="subscriptionModelItem"
-                v-on:model-choose-plan="/*saveInformationPlan*/"
-                v-bind:clientId="clientId"
+                v-on:model-choose-plan="saveInformationPlan"
             />
             <CardPlan 
                 v-bind:item="plans[2]" 
                 v-bind:subscription="subscriptionModelItem"
-                v-on:model-choose-plan="/*saveInformationPlan*/"
-                v-bind:clientId="clientId"
+                v-on:model-choose-plan="saveInformationPlan"
             /> 
         </v-row>
     </v-container>
@@ -25,13 +22,13 @@
 
 <script>
 import PlansApiService from "../../core/services/plans-api-service";
-//import MyPlansApiService from "../../core/services/myplans-api-service";
+import ClientApiService from "../../core/services/clients-api-service";
 import CardPlan from "./CardPlan-model"
 
 export default{
     name:"ChoosePlan",
     props: {
-        clientId: String
+        clientId: Number
     },
     data(){
         return{
@@ -47,30 +44,30 @@ export default{
             return{
                 id: plan.id,
                 name: plan.name,
-                description: plan.description,
-                numArtefacts: plan.numArtefacts,
-                amount: plan.amount
+                price: plan.price
             }
         },
-        getPlans() {
-            PlansApiService.getAll()
-            .then(response => {
-                this.plans = response.data.map(this.getPlan)
-                console.log(this.plans)
-            })
+        async getPlans() {
+            await PlansApiService.getAll()
+              .then(response => {
+                  this.plans = response.data.map(this.getPlan)
+                  console.log(this.plans)
+              })
+              .catch(e => {
+                console.log(e);
+              });
         },
-        /*saveInformationPlan(modelPlan){
-            //console.log(modelPlan)
-            MyPlansApiService.create(modelPlan)
+        async saveInformationPlan(planId) {
+          await ClientApiService.updatePlan(this.clientId, planId)
             .then(response => {
-                console.log(response)
-                this.$emit("update-render")
-
+              console.log(response);
+              localStorage.setItem("client", JSON.stringify(response.data));
+              this.$emit("update-render");
             })
             .catch(e => {
-                console.log(e)
-            })
-        }*/
+              console.log(e);
+            });
+        }
     },
     mounted() {
         this.getPlans();
